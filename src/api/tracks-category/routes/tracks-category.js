732943255e003471ba4944e315027059234f178e
Alik Sloyan <alisloyan@GoldDigger.local>
2022-01-18 13:39:23 +0300

    Add Tracks and TracksCategories

---
+++ src/api/tracks-category/routes/tracks-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tracks-category router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tracks-category.tracks-category');
---
+++ src/api/tracks-category/services/tracks-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tracks-category service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tracks-category.tracks-category');
